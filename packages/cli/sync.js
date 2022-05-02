902f08fa1d069fda7d603839cc35bc76b673869d
Benjamin Herald <bherald@pm.me>
2022-05-02 13:59:48 +0300

    exclusion

--- packages/cli/sync.js
+++ packages/cli/sync.js
@@ -1,6 +1,6 @@
 
 const {RPC_RINKEBY, RPC_MUMBAI, KEY, CONTRACT_ADDRESS } = require('@list/config');
-const ListJSON = require("./List.json");
+const ListJSON = require("../contracts/abi/contracts/list.sol/List.json");
 const ethers = require("ethers");
 const newMemEmptyTrie = require("circomlibjs").newMemEmptyTrie;
 
@@ -29,10 +29,12 @@ async function main()
 	 	await tree.insert(record.adr, record.version);
 	 }
 	 
-	 const key = '0x8750A33948C11E21484fb21e9Ff2D0e238e8527f';
-	 const result = await tree.find(key);
-	 console.log("Tree root", tree.root, "found", result);
-	 await testInclusion(tree, key);
+	 const key1 = '0x8750A33948C11E21484fb21e9Ff2D0e238e8527f';
+	 const key2 = '0xbDEBAb0a14CDa02c196bDC2C2490D96d3DfC6a61';
+
+	 console.log("Tree root", tree.root);
+	 await generateInput(tree, key1);
+	 await generateInput(tree, key2);
 	 
 };
 
@@ -43,21 +45,21 @@ async function generateInput(tree, _key)
     const key = tree.F.e(_key);
     const res = await tree.find(key);
 
-    console.log("testInclusion res.found", res.found, "for key", _key);
+    console.log("Key found:", res.found, "for key", _key);
     let siblings = res.siblings;
     for (let i=0; i<siblings.length; i++) siblings[i] = tree.F.toObject(siblings[i]);
     while (siblings.length<10) siblings.push(0);
 
     const input = {
 			enabled: 1,
-			fnc: 0,
+			fnc: res.found? 0 : 1,
 			root: tree.F.toObject(tree.root),
 			siblings: siblings,
-			oldKey: 0,
-			oldValue: 0,
-			isOld0: 0,
+			oldKey: res.found? 0 : (res.isOld0 ? 0 : tree.F.toObject(res.notFoundKey)),
+			oldValue: res.found? 0 : (res.isOld0 ? 0 : tree.F.toObject(res.notFoundValue)),
+			isOld0: res.found? 0 : (res.isOld0 ? 1 : 0),
 			key: tree.F.toObject(key),
-			value: tree.F.toObject(res.foundValue)
+			value: res.found? tree.F.toObject(res.foundValue) : 0
 		};
 
     
